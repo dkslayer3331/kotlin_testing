7128d2c11bd344a606b98780412a2f88352d3df5
moehtet <laxus3331@gmail.com>
2019-07-28 18:30:34 +0630

    x

---
+++ app/src/main/java/com/nwt/first_kotlin_test/MovieDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.nwt.first_kotlin_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
