566cabc820408104409659800ebe589d9e2b0c8e
moehtet <laxus3331@gmail.com>
2019-08-01 22:28:15 +0630

    actor detail

---
+++ app/src/main/java/com/nwt/first_kotlin_test/Activities/ActorDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.nwt.first_kotlin_test.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nwt.first_kotlin_test.R
+
+class ActorDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actor_detail)
+    }
+}
